80028dd67efcff560820d1e57c5b441d9b2a51de
Guney Bilen <guneybilen@yahoo.com>
2012-10-06 10:15:09 +0300

    added title helper to application_helper and corrected a glitch in sortable method call about t() method in tabler.html.erb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
+
+
+  def title
+    base_title = 'isgetir.com'
+
+    if !@title.nil?
+      "#{base_title} | #{@title}"
+    else
+      base_title
+    end
+  end
+
   # Creates a submit button with the given name with a cancel link
   # Accepts two arguments: Form object and the cancel link name
   def submit_or_cancel(form, name='Cancel')
@@ -19,6 +31,7 @@ module ApplicationHelper
 
 def sortable(column, title = nil, klass)
   title ||= column.titleize
+  puts title
   css_class = column == sort_column ? "current #{sort_direction}" : nil
   direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
   link_to title, params.merge(:sort => column, :direction => direction, :page=>nil), {:class => css_class}
