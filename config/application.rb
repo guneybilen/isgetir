78aa10dfae0decc032c27457c8bf77c314753e43
Guney Bilen <guneybilen@yahoo.com>
2013-11-23 15:20:51 +0200

    a simple comment in application.rb

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Isgetir
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false  # without this line heroku gives 'precompile assets failed' error
 
     config.generators do |g|
       g.test_framework :rspec
