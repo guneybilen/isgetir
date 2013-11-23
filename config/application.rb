caddb4090d09859cb7b539ce370c20b732a6ddb0
Guney Bilen <guneybilen@yahoo.com>
2013-11-23 15:15:58 +0200

    config.assets.initialize_on_precompile = false in application.rb

--- config/application.rb
+++ config/application.rb
@@ -58,8 +58,10 @@ module Isgetir
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-  config.generators do |g|
-    g.test_framework :rspec
-  end
+    config.assets.initialize_on_precompile = false
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
